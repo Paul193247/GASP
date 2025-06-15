e8cbd92e4a2855f16118b840dfae0b2a2ddc7bb2
Paul193247 <121023613+Paul193247@users.noreply.github.com>
2025-06-15 12:45:21 +0200

    download

--- langfassungen/script.js
+++ langfassungen/script.js
@@ -1,1 +1,12 @@
 const button = document.getElementById("download");
+
+button.addEventListener("click", function (e) {
+  const link = document.createElement("a");
+  link.href = window.location.pathname.replace(".html", ".pdf"); // passe den Pfad an
+  link.download = window.location.pathname
+    .replace(".html", ".pdf")
+    .split("/")[2]; // der gewünschte Dateiname
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
